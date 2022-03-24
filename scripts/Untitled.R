a819e23c84c2360c79bd230ea2850e423137fc17
Ana Mamatelashvili <>
2022-03-24 10:51:51 +0400

    Update sample script

--- scripts/Untitled.R
+++ scripts/Untitled.R
@@ -7,3 +7,13 @@ getwd()
 setwd("~/Documents/R_course/R_course")
 save.image()
 load(".Rdata")
+print(1:4)
+c(1:4)
+
+
+library(ggplot2)
+
+scatter <- ggplot(data=iris, aes(x = Sepal.Length, y = Sepal.Width)) 
+scatter + geom_point(aes(color=Species, shape=Species)) +
+  xlab("Sepal Length") +  ylab("Sepal Width") +
+  ggtitle("Sepal Length-Width")
